1a80b5b597f9b99c1d5ea9696df77758ff23ef25
Dylan McKee <dylan@djmckee.co.uk>
2016-11-04 14:11:22 +0000

    Added cat GIFs.

--- src/main/java/IndexServlet.java
+++ src/main/java/IndexServlet.java
@@ -12,7 +12,49 @@ public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.getWriter().print("<html><body><h3>Jetty Servlet works!</h3></body></html>");
+        response.getWriter().print(
+                "<html><body><h3>Enter username:</h3><form action=\"index\" and method=\"post\">\n" +
+                        "  Username:<br>\n" +
+                        "  <input type=\"text\" name=\"username\"><br>\n"
+                        + "<input type=\"checkbox\" name=\"catgifs\" value=\"catgifs\"> Cat gifs?<br>"
+                        + "   <input type=\"submit\" value=\"Submit\">\n" +
+                        "</form></body></html>"
+        );
 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Perform form POST request...
+        String username = request.getParameter("username");
+        String catGifsString = request.getParameter("catgifs");
+
+        boolean showCatGifs = false;
+
+        if (catGifsString != null) {
+            if (catGifsString.length() > 0) {
+                showCatGifs = true;
+            }
+        }
+
+
+        String catGifsHtml = "";
+
+        String[] gifUrls = {
+                "https://media.giphy.com/media/GvMSpPx44XlFm/giphy.gif",
+                "https://media.giphy.com/media/JHCcEc9vLvHZS/giphy.gif",
+                "https://media.giphy.com/media/idwespBy17WMw/giphy.gif",
+                "https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif"
+        };
+
+        int randomNumber =  (int)(Math.random() * gifUrls.length);
+
+        String gifUrl = gifUrls[randomNumber];
+
+        if (showCatGifs) {
+            catGifsHtml = "<img alt=\"Awesome cat gif\" src=\"" + gifUrl + "\" />";
+        }
+
+        response.getWriter().print("<html><body>Your username is: " + username  + "<br/><br/>" + catGifsHtml + "<br/></body></html>");
     }
 }
